7eb584680d3dd48086cb3c3cb1e97cdea02d2d11
harichandansingh <singh.harichandan@gmail.com>
2016-09-22 15:23:40 -0400

    First, and in this case, final commit

---
+++ AC32UnitMidunitMock/AC32UnitMidunitMock/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AC32UnitMidunitMock
+//
+//  Created by Harichandan Singh on 9/22/16.
+//  Copyright © 2016 Harichandan Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //HELLO WORLD - HARICHANDAN SINGH
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
