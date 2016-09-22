ad074bd01ebbe1c5fc2e590a706ec3123a03d6d6
Tom Seymour <tomseymour@Toms-MacBook-Pro.local>
2016-09-22 15:37:47 -0400

    This is Tom's first and maybe final commit

---
+++ AC32UnitMidunitMock/AC32UnitMidunitMock/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  AC32UnitMidunitMock
+//
+//  Created by Tom Seymour on 22/09/2016.
+//  Copyright © 2016 Tom Seymour. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // here is where I'm gonna write some code
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
