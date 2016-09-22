b00ffe742969881d4912b9e2cb8477ea7a225c40
Mira Estil <imz.estil@gmail.com>
2016-09-22 15:30:38 -0400

    Dickens code

---
+++ AC32UnitMidunitMock/AC32UnitMidunitMock/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AC32UnitMidunitMock
+//
+//  Created by Ilmira Estil on 9/22/16.
+//  Copyright © 2016 C4Q. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var quote: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
