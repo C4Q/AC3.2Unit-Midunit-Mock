c21ae012a503e1016bb2647fa712f83d27831b01
Kadell <Kadell@MacBook.local>
2016-09-22 15:34:02 -0400

    work done

---
+++ AC3.2Unit-Midunit-Mock/AC3.2Unit-Midunit-MockTests/AC3_2Unit_Midunit_MockTests.swift
@@ -0,0 +1,36 @@
+//
+//  AC3_2Unit_Midunit_MockTests.swift
+//  AC3.2Unit-Midunit-MockTests
+//
+//  Created by Kadell on 9/22/16.
+//  Copyright © 2016 Kadell. All rights reserved.
+//
+
+import XCTest
+@testable import AC3_2Unit_Midunit_Mock
+
+class AC3_2Unit_Midunit_MockTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
